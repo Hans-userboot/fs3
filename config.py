58b9648024c26458f385423f0805440478a07393
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-04-20 06:58:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://txxsmyof:b99F5n0mIL1ND8-uGHS
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
