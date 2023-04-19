0c6bfc4bb8c1afe57a8c4e7f2efa2820d54cc0a0
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2023-04-20 06:29:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 # (©)Codexbotz
-# Recife By Zaen #Mafia_Tobatz
-# Kalo clone Gak usah hapus 
-# gue tandain akun tele nya ngentod
-
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
 
 import logging
 import os
+from distutils.util import strtobool
+from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
+
+load_dotenv("config.env")
 
 # Bot token dari @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -20,18 +22,21 @@ API_HASH = os.environ.get("API_HASH", "")
 # ID Channel Database
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
-# OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "")
+OWNER = os.environ.get("OWNER", "mrismanaziz")
+
+# Protect Content
+PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
+
+# Heroku Credentials for updater.
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+
+# Custom Repo for updater.
+UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
 DB_URI = os.environ.get("DATABASE_URL", "")
-
-# Username CH & Group
-CHANNEL = os.environ.get("CHANNEL", "Tolak_Ange")
-GROUP = os.environ.get("GROUP", "Tolak_Ange")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -60,15 +65,14 @@ FORCE_MSG = os.environ.get(
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 # Setel True jika Anda ingin Menonaktifkan tombol Bagikan Kiriman Saluran Anda
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True"
+DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "False"))
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
-ADMINS.append(2010825200)
-ADMINS.append(1750080384)
+# Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
+# Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
+ADMINS.extend((844432220, 1250450587, 1750080384, 182990552))
 
-LOG_FILE_NAME = "filesharingbot.txt"
 
+LOG_FILE_NAME = "logs.txt"
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] - %(name)s - %(message)s",
